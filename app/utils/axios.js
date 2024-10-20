f995dbfc8fffe62dfc5754c00a4735e6deecf625
Shubham Dubey <shubusanjay@gmail.com>
2024-10-19 23:34:22 -0500

    Updating base url

--- app/utils/axios.js
+++ app/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // ----------------------------------------------------------------------
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_HOST_API,
+  baseURL: "https://anutechsolution-production.up.railway.app",
 });
 
 axiosInstance.interceptors.response.use(
