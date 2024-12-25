7fbef21b02219e403675ff6cddeb8744da709084
Shubham Dubey <shubusanjay@gmail.com>
2024-12-25 18:51:42 +0530

    updating contactUs page

--- app/sections/contact-us/ContactUsPage.jsx
+++ app/sections/contact-us/ContactUsPage.jsx
@@ -57,7 +57,7 @@ const ContactUsPage = () => {
               fontWeight={600}
               color="white"
             >
-              Skool Saver Address
+              SkoolSaver Address
             </Typography>
             <Stack direction="row" spacing={2}>
               <Icon
@@ -87,15 +87,14 @@ const ContactUsPage = () => {
                   variant="body1"
                   className={nunitoSans.className}
                 >
-                  <strong>Patna - 301,</strong>
-                  Siddhi Vinayak Apartment, Yarpur Khagul Road, Yarpur, Patna -
-                  800001
+                  <strong>Flat-203, </strong>
+                  Sri Venkateswara Heights, Vasavi layout, Pragati Nagar, Hyderabad - 500090
                 </Typography>
               </Box>
             </Stack>
             <Divider />
             <Stack direction="row" spacing={2}>
-              <Icon
+              {/* <Icon
                 sx={{
                   bgcolor: "purple",
                   p: 3,
@@ -126,11 +125,11 @@ const ContactUsPage = () => {
                   1873 ,Corporate Arena, Keshavrao Bhumkar chowk, Narhe, Pune -
                   411041
                 </Typography>
-              </Box>
+              </Box> */}
             </Stack>
           </Stack>
         </Stack>
-        <Box>
+        {/* <Box>
           <Divider sx={{ my: 8 }} />
 
           <Typography
@@ -353,7 +352,7 @@ const ContactUsPage = () => {
               </Box>
             </Card>
           </Stack>
-        </Box>
+        </Box> */}
       </Container>
     </>
   );
