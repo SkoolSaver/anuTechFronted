5281d5d6755aa58be0c5a715e450b5158fbc4204
AryanDhillu <dhilluaryan7@gmail.com>
2025-10-18 16:05:43 +0530

    updated tnc

--- app/sections/contact-us/ContactUsPage.jsx
+++ app/sections/contact-us/ContactUsPage.jsx
@@ -1,16 +1,6 @@
 import ContactUsForm from "@/app/components/contact-us/ContactUsForm";
 import { nunitoSans, popins } from "@/app/google-fonts/fonts";
-import HomeIcon from "@mui/icons-material/Home";
-import LocationOnIcon from "@mui/icons-material/LocationOn";
-import {
-  Box,
-  Card,
-  Container,
-  Divider,
-  Icon,
-  Stack,
-  Typography,
-} from "@mui/material";
+import { Box, Card, Container, Stack, Typography } from "@mui/material";
 import React from "react";
 import MainTitle from "../common/MainTitle";
 
@@ -59,11 +49,7 @@ const ContactUsPage = () => {
             </Box>
           </Card>
 
-          <Stack 
-            width={{ xs: "100%", md: "45%" }} 
-            spacing={6} 
-            pt={5}
-          >
+          <Stack width={{ xs: "100%", md: "45%" }} spacing={4} pt={5}>
             <Typography
               variant="h3"
               fontSize={35}
@@ -71,204 +57,29 @@ const ContactUsPage = () => {
               fontWeight={600}
               color="white"
             >
-              SkoolSaver Address
+              Contact Information
             </Typography>
-            <Stack direction="row" spacing={2}>
-              <Icon
-                sx={{
-                  bgcolor: "orange",
-                  p: 3,
-                  borderRadius: "50%",
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <HomeIcon sx={{ fontSize: 25, color: "white" }} />
-              </Icon>
-              <Box display="flex" flexDirection="column" gap={2}>
-                <Typography
-                  variant="h3"
-                  className={popins.className}
-                  fontWeight={600}
-                  fontSize={18}
-                  color="white"
-                >
-                  Registered office
-                </Typography>
-                <Typography
-                  color="white"
-                  variant="body1"
-                  className={nunitoSans.className}
-                >
-                  <strong>Flat-203, </strong>
-                  Sri Venkateswara Heights, Vasavi layout, Pragati Nagar, Hyderabad - 500090
-                </Typography>
-                {/* Added mail details */}
-                <Typography
-                  color="white"
-                  variant="body1"
-                  className={nunitoSans.className}
-                >
-                  <strong>Email: </strong>	skoolsaverclub.in
-                </Typography>
-              </Box>
-            </Stack>
 
-            <Divider sx={{ my: 4 }} />
-
-            {/* Optional: Regional Offices */}
-            <Stack direction="row" spacing={2}>
-              {/* Lucknow Office */}
-              {/* <Card
-                sx={{
-                  display: "flex",
-                  bgcolor: "black",
-                  p: 4,
-                  transition: "transform 0.3s ease",
-                  border: "1px solid white",
-                  gap: 2,
-                  maxWidth: 350,
-                  "&:hover": {
-                    background: "#0f0f0f",
-                    transform: "scale(1.05)",
-                  },
-                }}
-              >
-                <Icon
-                  sx={{
-                    bgcolor: "orange",
-                    p: 3,
-                    borderRadius: "50%",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  <LocationOnIcon sx={{ fontSize: 25, color: "white" }} />
-                </Icon>
-                <Box>
-                  <Typography
-                    variant="h3"
-                    color="white"
-                    fontSize={18}
-                    className={popins.className}
-                    fontWeight={600}
-                  >
-                    Lucknow
-                  </Typography>
-                  <Typography
-                    variant="body1"
-                    fontSize={16}
-                    className={nunitoSans.className}
-                    mt={2}
-                    color="white"
-                  >
-                    Sadhna Bhavan, 1st Floor, L-3/83, Sector-D, Aliganj, Pincode-226024, Uttar-Pradesh, Landmark: Near UPPSC Building.
-                  </Typography>
-                </Box>
-              </Card> */}
-
-              {/* Bangalore Office */}
-              {/* <Card
-                sx={{
-                  display: "flex",
-                  bgcolor: "black",
-                  border: "1px solid white",
-                  p: 4,
-                  transition: "transform 0.3s ease",
-                  gap: 2,
-                  maxWidth: 350,
-                  "&:hover": {
-                    background: "#0f0f0f",
-                    transform: "scale(1.05)",
-                  },
-                }}
-              >
-                <Icon
-                  sx={{
-                    bgcolor: "purple",
-                    p: 3,
-                    borderRadius: "50%",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  <LocationOnIcon sx={{ fontSize: 25, color: "white" }} />
-                </Icon>
-                <Box>
-                  <Typography
-                    variant="h3"
-                    fontSize={18}
-                    color="white"
-                    className={popins.className}
-                    fontWeight={600}
-                  >
-                    Bangalore
-                  </Typography>
-                  <Typography
-                    variant="body1"
-                    fontSize={16}
-                    color="white"
-                    className={nunitoSans.className}
-                    mt={2}
-                  >
-                    Chetan Murty, #300, Forest Layout, RVCE Post, Bangalore – 560059, Karnataka
-                  </Typography>
-                </Box>
-              </Card> */}
-
-              {/* Pune Office */}
-              {/* <Card
-                sx={{
-                  display: "flex",
-                  bgcolor: "black",
-                  border: "1px solid white",
-                  p: 4,
-                  transition: "transform 0.3s ease",
-                  gap: 2,
-                  maxWidth: 350,
-                  "&:hover": {
-                    background: "#0f0f0f",
-                    transform: "scale(1.05)",
-                  },
-                }}
-              >
-                <Icon
-                  sx={{
-                    bgcolor: "palevioletred",
-                    p: 3,
-                    borderRadius: "50%",
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                >
-                  <LocationOnIcon sx={{ fontSize: 25, color: "white" }} />
-                </Icon>
-                <Box>
-                  <Typography
-                    variant="h3"
-                    fontSize={18}
-                    className={popins.className}
-                    fontWeight={600}
-                    color="white"
-                  >
-                    Pune
-                  </Typography>
-                  <Typography
-                    variant="body1"
-                    color="white"
-                    fontSize={16}
-                    className={nunitoSans.className}
-                    mt={2}
-                  >
-                    1873, Corporate Arena, Keshavrao Bhumkar Chowk, Narhe, Pune – 411041, Maharashtra
-                  </Typography>
-                </Box>
-              </Card> */}
-            </Stack>
+            <Box>
+              <Typography color="white" className={nunitoSans.className} mb={2}>
+                You may contact us using the information below:
+              </Typography>
+              <Typography color="white" className={nunitoSans.className}>
+                <strong>Merchant Legal entity name:</strong> SKOOLSAVER PRIVATE LIMITED
+              </Typography>
+              <Typography color="white" className={nunitoSans.className} mt={1}>
+                <strong>Registered Address:</strong> Flat 203, Sri Venkateswara Heights, Vasavai Layout, Pragatinagar, Hyderabad, Telangana, India, PIN: 500090
+              </Typography>
+              <Typography color="white" className={nunitoSans.className} mt={1}>
+                <strong>Operational Address:</strong> Flat 203, Sri Venkateswara Heights, Vasavai Layout, Pragatinagar, Hyderabad, Telangana, India, PIN: 500090
+              </Typography>
+              <Typography color="white" className={nunitoSans.className} mt={1}>
+                <strong>Telephone No:</strong> 8309782214
+              </Typography>
+              <Typography color="white" className={nunitoSans.className} mt={1}>
+                <strong>E-Mail ID:</strong> gopi@skoolsaver.com
+              </Typography>
+            </Box>
           </Stack>
         </Stack>
       </Container>
